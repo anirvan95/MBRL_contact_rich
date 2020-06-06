23764a2cac21ddcc5f548af3e6c92bfa80240868
Anirvan <anirvan.dutta95@gmail.com>
2020-06-06 20:26:13 +0200

    Added the log files for the experiments for base_policy, model_based and model_free option critic

--- run_ioc_policy.py
+++ run_ioc_policy.py
@@ -6,12 +6,12 @@ import gym
 import os
 
 
-def train(env_id, num_iteration, seed, model_path=None):
+def train(env_id, num_iteration, seed, log_path=None, model_path=None):
     # Create TF session
     U.make_session().__enter__()
 
     def policy_fn(name, ob_space, ac_space):
-        return ioc_model.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space, hid_size=64, num_hid_layers=2, num_options=2, dc=0, w_intfc=True, k=0)
+        return ioc_model.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space, hid_size=32, num_hid_layers=2, num_options=2, dc=0, w_intfc=True, k=0)
 
     # Create Mujoco environment
     env = gym.make(env_id)
@@ -24,7 +24,7 @@ def train(env_id, num_iteration, seed, model_path=None):
     pi = ioc_runner.learn(env, model_path, policy_fn, num_options=2,
                           horizon=150, batchSize=int(150 * 80),  # timesteps per actor per update
                           clip_param=0.2, entcoeff=0.02,
-                          optim_epochs=50, optim_stepsize=2.75e-4, optim_batchsize=32,  # optimization hypers
+                          optim_epochs=30, optim_stepsize=2.5e-4, optim_batchsize=32,  # optimization hypers
                           gamma=0.99, lam=0.95,  # advantage estimation
                           max_timesteps=3e6, max_episodes=0, max_iters=num_iteration, max_seconds=0,  # time constraint
                           callback=None,  # you can do anything in the callback, since it takes locals(), globals()
@@ -34,8 +34,8 @@ def train(env_id, num_iteration, seed, model_path=None):
                           )
     env.close()
     if model_path:
-        # U.save_state(model_path)
-        print("Model saving skipped")
+        U.save_state(log_path)
+        print("Model saved")
 
     return pi
 
@@ -45,19 +45,19 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', type=str, default='Block2D-v1')
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num_iteration', type=float, default=150)
+    parser.add_argument('--num_iteration', type=float, default=50)
     parser.add_argument('--model_path', help='Path to save trained model to',
                         default=os.path.join(logger.get_dir(), 'block_ppo'), type=str)
     parser.add_argument('--log_path', help='Directory to save learning curve data.', default=None, type=str)
     parser.add_argument('--play', default=False, action='store_true')
-    parser.add_argument('--horizon', help='Maximum time horizon in each iteration', default=150, type=int)
+    parser.add_argument('--horizon', help='Maximum time horizon in each iteration', default=50, type=int)
 
     args = parser.parse_args()
     logger.configure(dir=args.log_path)
 
     if not args.play:
         # Train the Model
-        train(args.env, num_iteration=args.num_iteration, seed=args.seed, model_path=args.model_path)
+        train(args.env, num_iteration=args.num_iteration, seed=args.seed, log_path=args.log_path, model_path=args.model_path)
     else:
         # Load the saved model for demonstration
         pi = train(args.env, num_iteration=1, seed=args.seed)
